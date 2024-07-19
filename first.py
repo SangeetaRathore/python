d14b31e28a509b7084a031751c44cbde19b6fc52
SangeetaRathore <sangeetarathore23march@gmail.com>
2024-07-19 20:55:08 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,6 @@
+print("Hello world")
+
+def greet(s):
+         print("hello" , s)
+
+greet("san")
---
+++ first_one.py
@@ -0,0 +1,5 @@
+from first import greet
+
+greet("rathore")   #there  whole first.py file  arecome. so it give output of first and first_one file
+
+cd .
